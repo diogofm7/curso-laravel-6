2ceaa0971fb46bec61c8d39002ce1ce3d624af49
diogofm7 <33474923+diogofm7@users.noreply.github.com>
2019-12-07 16:27:04 -0300

    Criação de Rotas

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,38 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contato', function (){
+    return view('site.contact');
+});
+
+Route::get('/empresa', function (){
+    return 'Sobre a Empresa';
+});
+
+Route::post('/register', function(){
+    return '';
+});
+
+/* Route::any aceita todos os tipos de requisição HTTP (GET, POST, ...), TOMAR CUIDADO AO USAR */
+Route::any('/any', function(){
+    return 'Any';
+});
+
+/* Route::match indica quais os tipos de requisição HTTP irá aceita */
+Route::match(['get', 'post'], '/match', function(){
+    return 'Match';
+});
+
+/* URL Dinamica */
+Route::get('/categorias/{flag1}', function ($prm1){
+    return "Produtos da categoria: {$prm1}";
+});
+
+Route::get('/categoria/{flag}/posts', function ($prm1){
+    return "Posts da categoria: {$prm1}";
+});
+
+Route::get('/produtos/{idProduct?}', function ($idProduct = ''){
+    return "Produto(s) {$idProduct}";
+});
