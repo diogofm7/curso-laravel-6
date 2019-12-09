48978c972a707654ada5cde9127d7015a95ff063
diogofm7 <33474923+diogofm7@users.noreply.github.com>
2019-12-09 00:17:13 -0300

    Finalização da parte de Rotas

--- routes/web.php
+++ routes/web.php
@@ -49,3 +49,77 @@ Route::get('/categoria/{flag}/posts', function ($prm1){
 Route::get('/produtos/{idProduct?}', function ($idProduct = ''){
     return "Produto(s) {$idProduct}";
 });
+
+/*
+Route::get('redirect1', function (){
+    return redirect('redirect2');
+});
+*/
+Route::redirect('redirect1', 'redirect2');
+
+Route::get('redirect2', function (){
+    return 'Redirect 02';
+});
+
+/*
+Route::get('view', function () {
+    return view('welcome');
+});
+*/
+Route::view('view', 'welcome');
+
+Route::get('redirect3', function (){
+    return redirect()->route('url.name');
+});
+
+Route::get('nome-url', function (){
+   return 'Hey hey hey';
+})->name('url.name');
+
+/*
+Route::middleware([])->group(function (){
+
+    Route::prefix('admin')->group(function (){
+
+        Route::namespace('Admin')->group(function (){
+
+            Route::name('admin.')->group(function (){
+                Route::get('dashboard', 'TesteController@teste')->name('dashboard');
+
+                Route::get('financeiro', 'TesteController@teste')->name('financeiro');
+
+                Route::get('produtos', 'TesteController@teste')->name('produtos');
+
+                Route::get('/', function (){
+                    return redirect()->route('admin.dashboard');
+                })->name('home');
+            });
+
+        });
+
+    });
+
+});
+*/
+Route::group([
+    'middleware' => [],
+    'prefix' => 'admin',
+    'namespace' => 'Admin',
+    'as' => 'admin.'
+], function (){
+    Route::get('dashboard', 'TesteController@teste')->name('dashboard');
+
+    Route::get('financeiro', 'TesteController@teste')->name('financeiro');
+
+    Route::get('produtos', 'TesteController@teste')->name('produtos');
+
+    Route::get('/', function (){
+        return redirect()->route('admin.dashboard');
+    })->name('home');
+});
+
+
+
+Route::get('/login', function (){
+    return 'login';
+})->name('login');
