2109f3f48960e37326546af75e4cd827220584f9
Diogo Ferreira <diogo.f.m.7@gmail.com>
2021-04-14 19:44:29 -0300

    commit

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        factory(User::class, 10)->create();
+
+//        User::create([
+//            'name' => 'Diogo Ferreira',
+//            'email' => 'diogo.f.m.7@gmail.com',
+//            'password' => bcrypt('12345678') //12345678
+//        ]);
+//
+    }
+}
